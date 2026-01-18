40117ff2227c49dfcd7ac348815ddf5ff34baaad
unknown <jatin.careeer@gmail.com>
2026-01-18 20:06:47 +0530

    more pratice

--- Advanced Django/myProject18/student/urls.py
+++ Advanced Django/myProject18/student/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('add/',views.student_create,name='student_create'),
+    path('',views.Student_list,name='student_list'),
+    path('details/<int:pk>/',views.student_detail,name='student_detail')
 ]
