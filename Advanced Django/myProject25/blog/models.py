992c0665966d26348652ccc526a8d7883bf5e593
unknown <jatin.careeer@gmail.com>
2026-01-21 22:34:54 +0530

    UP

---
+++ Advanced Django/myProject25/blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.urls import reverse
+
+class Post(models.Model):
+    title=models.CharField()
+    content=models.TextField()
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("post_detail",args=[str(self.id)])
+    
+
+
